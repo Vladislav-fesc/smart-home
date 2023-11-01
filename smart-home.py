31b7d88dd8225969db74a66b5038dc38ba4bd026
Vlad <feschukovva@gmail.com>
2023-11-01 12:02:23 +0700

    smart-home

--- smart-home.py
+++ smart-home.py
@@ -1,4 +1,12 @@
-print("hello")
-print(1223)
-print (11111)
-print(4444)
+'''import datetime
+def Signal:
+    if signal == True:
+    Выключаем все кроме датчика воды в ваной 
+   '''
+    
+'''def time:
+    current_time = datetime.datetime.now().time()
+    if urrent_time >= datetime.time(22, 0) or current_time <= datetime.time(6, 0):
+            выключаем свет во всем доме и кондецианеры 
+
+       '''
