c82e961554c4f683fa1709b0f0f84dd43192ff5f
Vlad <feschukovva@gmail.com>
2023-11-02 20:48:06 +0700

    smart-home

--- smart-home.py
+++ smart-home.py
@@ -83,7 +83,35 @@ class tempertur:
         else:
             print("выключить кондиционер")
             self.air_conditioner1 = False
+class WindowSensor:
+    def __init__(self,com):
+        self.window_open = com  # Изначально окно закрыто
+        self.air_conditioner = False  # Кондиционер изначально выключен
+
+    def window(self):
+        if self.window_open == True:
+            print("окно открыто")
+            self.turn_off_air_conditioner()
+        else:
+            print("окно закрыто")
+            self.turn_on_air_conditioner()
+
+    def turn_on_air_conditioner(self):
+        self.air_conditioner = True
+        print(" кондиционер работает")
+
+    def turn_off_air_conditioner(self):
+        self.air_conditioner = False
+        print("выключить кондиционер")
+
+    def monitor_window(self):
+        if self.window_open:
+            self.window()  # Если окно открыто, выключаем кондиционер
+        else:
+             self.window() # Если окно закрыто, включаем кондиционер
 test_list = [True,False]
+window_sensor = WindowSensor(test_list[random.randint(0,1)])
+window_sensor.monitor_window() 
 my_home = signall(test_list[random.randint(0,1)])
 my_home.off()
 temper = tempertur(13,14,15,16)
