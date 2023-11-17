892f987c1ec3c0373b791814bf3a6eb0cb4959cd
Vlad <feschukovva@gmail.com>
2023-11-17 23:28:55 +0700

    Flask

--- Flask/app.py
+++ Flask/app.py
@@ -14,17 +14,16 @@ sensor_data = {
     'temperature4': 23.5
 }
 
-light_status = False
 temperature_history = []
 time_history = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', sensor_data=sensor_data, light_status=light_status)
+    return render_template('index.html', sensor_data=sensor_data)
 
 @app.route('/update_data', methods=['POST'])
 def update_data():
-    global temperature_history, time_history, light_status
+    global temperature_history, time_history
 
     # Обновление данных с датчиков
     for sensor in sensor_data:
@@ -32,20 +31,22 @@ def update_data():
 
     # Добавление новых данных в историю
     current_time = datetime.now().strftime('%H:%M:%S')
-    time_history.append(current_time)
-    temperature_history.append(sensor_data['temperature1'])
+    temperature_history.append({
+        'time': current_time,
+        'temperature1': sensor_data['temperature1'],
+        'temperature2': sensor_data['temperature2'],
+        'temperature3': sensor_data['temperature3'],
+        'temperature4': sensor_data['temperature4']
+    })
 
     # Ограничение истории в 20 точках
     if len(temperature_history) > 20:
         temperature_history = temperature_history[-20:]
-        time_history = time_history[-20:]
-
-    # Обновление статуса света (для примера, используем случайное значение)
-    light_status = not light_status
 
     # Возвращение данных в формате JSON для обновления веб-страницы
-    return jsonify({'sensor_data': sensor_data, 'light_status': light_status, 'time_history': time_history, 'temperature_history': temperature_history})
+    return jsonify({'sensor_data': sensor_data, 'temperature_history': temperature_history})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
