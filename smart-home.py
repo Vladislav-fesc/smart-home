dc4e6428430fbc6dc44237901e17624f2dac32e4
Vlad <feschukovva@gmail.com>
2023-11-18 10:33:33 +0700

    Flask

--- smart-home.py
+++ smart-home.py
@@ -3,6 +3,7 @@ from Class.Signall import signall
 from Class.ClimateControl import tempertur
 from Class.WindowSensor import WindowSensor
 from Class.Datchik.temperatur import TemperatureSensor
+from Flask.app import flask
 test_list = [True,False]
 window_sensor = WindowSensor(test_list[random.randint(0,1)])
 window_sensor.monitor_window() 
@@ -11,3 +12,6 @@ my_home.off()
 temper = TemperatureSensor(1, 'TESET', 5)
 temper1 = tempertur(temper.measure(),0,0,0)
 temper1.temperatur()
+flaske =flask()
+flaske.flsk()
+
